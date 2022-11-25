cf12767516309fd23e184c8334d4cf2da11caf5a
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-25 14:18:47 +0100

    Completed Exercise + Bonus

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        title: "Hello to Everyone!",
+        myClass: "red",
+        myImage: "media", 
+        myMedia: "https://unsplash.it/600/300?image=171"
+      }
+    }
+  }).mount("#myApp")
